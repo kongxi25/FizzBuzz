5d592c96d6e399ca7c37bb19ce06fcadf7f7e3ea
Sean Kong <x.kong@mail.utoronto.ca>
2023-09-14 20:31:06 -0400

    everything works

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         FizzBuzz.doFizzBuzz(5);
+
+        String[] x = {"hoopla", "spoopy", "weirdo"};
+
+        FizzBuzz.main(x);
+
     }
 }
